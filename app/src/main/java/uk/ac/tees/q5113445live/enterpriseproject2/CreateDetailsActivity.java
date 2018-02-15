744774381c1790bd58c3f505a4834638b112769b
James LIDDLE <jameslid12@msn.com>
2018-02-15 21:23:25 +0000

    Created Activity for Delivery

---
+++ app/src/main/java/uk/ac/tees/q5113445live/enterpriseproject2/CreateDetailsActivity.java
@@ -0,0 +1,13 @@
+package uk.ac.tees.q5113445live.enterpriseproject2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_details);
+    }
+}
