ba3feb1d5d0e5788854e3f23d4c5b7ba3c3a3e9d
James LIDDLE <jameslid12@msn.com>
2018-02-15 21:23:55 +0000

    Created Activity for Delivery

--- app/src/main/java/uk/ac/tees/q5113445live/enterpriseproject2/CreateDetailsActivity.java
+++ app/src/main/java/uk/ac/tees/q5113445live/enterpriseproject2/CreateDetailsActivity.java
@@ -1,10 +1,24 @@
 package uk.ac.tees.q5113445live.enterpriseproject2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateDetailsActivity extends AppCompatActivity {
-
+    public Button but2;
+    public  void init()
+    {
+        but2= (Button)findViewById(R.id.button);
+        but2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toy = new Intent(CreateDetailsActivity.this,HomeActivity.class);
+                startActivity(toy);
+            }
+        });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/uk/ac/tees/q5113445live/enterpriseproject2/HomeActivity.java
+++ app/src/main/java/uk/ac/tees/q5113445live/enterpriseproject2/HomeActivity.java
@@ -1,13 +1,30 @@
 package uk.ac.tees.q5113445live.enterpriseproject2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity
+{
 
+    public Button but1;
+    public  void init()
+    {
+        but1= (Button)findViewById(R.id.button);
+        but1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toy = new Intent (HomeActivity.this,CreateDetailsActivity.class);
+                startActivity(toy);
+            }
+        });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home2);
+        init();
     }
 }
